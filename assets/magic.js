42de3226345da22f65511342142fd60f232bfedf
Armando Ugalde <uarmando15@yahoo.com>
2022-04-22 23:48:26 -0700

    added a save icon svg

--- assets/magic.js
+++ assets/magic.js
@@ -1,12 +1,18 @@
 function maketimeblocks(hour, existingtodo="") {
+    var currenttime = new Date().getHours();
+    var timetense = "future";
+    if(currenttime > hour) timetense= "past";
+    if(currenttime === hour) timetense = "present";
 $(".container").append($(`
 <div class="row time-block">
 <div class="hour col-1">${hour}:00</div>
-<textarea name="" id="" cols="30" rows="3" class="description col-9" ${existingtodo}></textarea>
-<div class="btn saveBtn col-2"></div>
+<textarea name="" id="" cols="30" rows="3" class="description col-9 ${timetense}"${existingtodo}></textarea>
+<div class="btn saveBtn col-2">
+<img src="/assets/save-solid.svg" width=10% </div>
 </div>`))
 }
 
-maketimeblocks();
 
-for(i = 9; i < 18; i++)
+for(i = 9; i < 18; i++) {
+    maketimeblocks(i);
+}
