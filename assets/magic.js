f62db55199d57dccb95fe958c675f87b7247b7ad
Armando Ugalde <uarmando15@yahoo.com>
2022-04-23 18:13:18 -0700

    attempting to debug failure to save to local storage

--- assets/magic.js
+++ assets/magic.js
@@ -1,12 +1,16 @@
+var savebtn = $(".saveBtn");
+var existingtodo = $("#todos").text();
 function maketimeblocks(hour, existingtodo="") {
     var currenttime = new Date().getHours();
     var timetense = "future";
     if(currenttime > hour) timetense= "past";
     if(currenttime === hour) timetense = "present";
+    var existingtodo= localStorage.getItem(hour);
+    if (!existingtodo) existingtodo="";
 $(".container").append($(`
 <div class="row time-block">
 <div class="hour col-1">${hour}:00</div>
-<textarea name="" id="" cols="30" rows="3" class="description col-9 ${timetense}"${existingtodo}></textarea>
+<textarea name="" id="${hour}" cols="30" rows="3" class="description col-9 ${timetense}">${existingtodo || ""}</textarea>
 <div class="btn saveBtn col-2">
 <img src="/assets/save-solid.svg" width=10% </div>
 </div>`))
@@ -16,3 +20,15 @@ $(".container").append($(`
 for(i = 9; i < 18; i++) {
     maketimeblocks(i);
 }
+function save(event) {
+    // localStorage.setItem("existingtodos", JSON.stringify(existingtodo));
+   var tododescription = event.target.parentNode.children[1].value
+   var todotime = event.target.parentNode.children[1].id
+
+   localStorage.setItem(todotime,tododescription);
+    }
+
+savebtn.on("click", function () {
+save();
+});
+console.log(savebtn.on)
