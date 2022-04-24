74ce663ce18a494511dbce44fe6780b6db8f26bf
Armando Ugalde <uarmando15@yahoo.com>
2022-04-23 19:54:22 -0700

    fixed issue with text not being saved to local storage

--- assets/magic.js
+++ assets/magic.js
@@ -1,5 +1,3 @@
-var savebtn = $(".saveBtn");
-var existingtodo = $("#todos").text();
 function maketimeblocks(hour, existingtodo="") {
     var currenttime = new Date().getHours();
     var timetense = "future";
@@ -7,19 +5,22 @@ function maketimeblocks(hour, existingtodo="") {
     if(currenttime === hour) timetense = "present";
     var existingtodo= localStorage.getItem(hour);
     if (!existingtodo) existingtodo="";
-$(".container").append($(`
-<div class="row time-block">
-<div class="hour col-1">${hour}:00</div>
-<textarea name="" id="${hour}" cols="30" rows="3" class="description col-9 ${timetense}">${existingtodo || ""}</textarea>
-<div class="btn saveBtn col-2">
-<img src="/assets/save-solid.svg" width=10% </div>
-</div>`))
+    $(".container").append($(`
+    <div class="row time-block">
+    <div class="hour col-1">${hour}:00</div>
+    <textarea name="" id="${hour}" cols="30" rows="3" class="description col-9 ${timetense}">${existingtodo || ""}</textarea>
+    <div class="btn saveBtn col-2">
+    <img src="/assets/save-solid.svg" width=10% </div>
+    </div>`))
 }
 
 
 for(i = 9; i < 18; i++) {
     maketimeblocks(i);
 }
+var savebtn = $(".saveBtn");
+var existingtodo = $("#todos").text();
+
 function save(event) {
     // localStorage.setItem("existingtodos", JSON.stringify(existingtodo));
    var tododescription = event.target.parentNode.children[1].value
@@ -28,7 +29,7 @@ function save(event) {
    localStorage.setItem(todotime,tododescription);
     }
 
-savebtn.on("click", function () {
-save();
+savebtn.on("click", function (event) {
+save(event);
 });
 console.log(savebtn.on)
